6dc3082d64108e54dd2095fc23044f11e0277af1
PunamchandBadwaik <badwaikpunamchand@gmail.com>
2015-07-21 20:06:27 +0530

    check once

--- src/com/dexpert/feecollection/main/payment/studentPayment/ApplicantFeeCollectionDAO.java
+++ src/com/dexpert/feecollection/main/payment/studentPayment/ApplicantFeeCollectionDAO.java
@@ -53,87 +53,126 @@ public class ApplicantFeeCollectionDAO {
 
 	}
 
+	// get List Of Service Types if exists
+	private List<ServiceTypeBean> getServiceTypeDetailList() {
+
+		Session session = factory.openSession();
+		try {
+			Criteria criteria = session.createCriteria(ServiceTypeBean.class);
+			List<ServiceTypeBean> list = criteria.list();
+			return list;
+		} finally {
+			session.close();
+			// TODO: handle exception
+		}
+
+	}
+
 	public Map<String, String> serviceTypeList() {
 
 		Map<String, String> map = new HashMap<String, String>();
 		List<ServiceTypeBean> list = new ArrayList<ServiceTypeBean>();
-		map.put("EC", "ELIGIBILITY CRITERIA");
-		map.put("ECR", "ELIGIBILITY CERTIFICATE RENEWAL");
-		map.put("CONAIE", "CHANGE OF NAME & INITIAL EXPANSION");
-		map.put("MC", "MIGRATION CERTIFICATE");
-		map.put("MT", "MIGRATION TRANSFER 1ST TO 2ND YEAR ");
-		map.put("NOCIT", "NOC(for Internship Transfer)");
-		map.put("CMCL", "CONSOLIDATED MARKS CARD (LAMINATED)");
-		map.put("NCIMC", "NAME CORRECTION IN MARKS CARD");
-		map.put("PDC", "PDC");
-		map.put("RC", "RANK CERTIFICATE");
-		map.put("MPC", "MEDAL/PRIZE CERTIFICATE");
-		map.put("MPCD", "MEDAL/PRIZE sCERTIFICATE-Duplicate");
-		map.put("DDC", "DUPLICATE DEGREE CERTIFICATE");
-		map.put("PGETS", "PGET Superspeciality");
-		map.put("PGETF", "PGET Fee");
-		map.put("PGETA", "PGET Application");
-		map.put("VO", "VERIFICATION ONLY");
-		map.put("VACOC", "VERIFICATION AND CERTIFICATION OF COPY'S");
-		map.put("CCOTSO", "CERTIFIED COPY OF THE SYLLABUS/ORINANCE");
-		map.put("DMC", "DUPLICATE MARKS CARD");
-		map.put("PPC", "PROVISIONAL PASS CERTIFICATE");
-		map.put("DC", "DEGREE CERTIFICATE");
-		map.put("CC", "CERTIFICATE COURSE");
-		map.put("NCIDC", "NAME CORRECTION IN DEGREE CERTIFICATE");
-		map.put("COTOTP", "CHANGE OF TITLE OF THESIS-PHD");
-		map.put("RRP", "RE-REGISTRATION-PH.D");
-		map.put("EOR", "EXTENSION OF REGISTRATION");
-		map.put("PE", "PRE EXAMINATION");
-		map.put("RF", "REGISTRATION FEE");
-		map.put("FSSAFPE", "FINAL SYNOPSIS SUBMISSION AND FINAL PH.D. EXAMINATION");
-		map.put("DF", "DISSERTATION FEE");
-		map.put("DFR", "DISSERTATION FEE-RESUBMISSION");
-		map.put("CVOTAOTDF", "CREDENTIAL VERIFICATION / OFFICIAL TRANSCRIPT/ ATTESTATION OF THE DOCUMENTS FEE");
-		map.put("MOIEC", "MEDIUM OF INSTRUCTION IN ENGLISH CERTIFICATION");
-		map.put("MLCPFR2500", "MALPRACTICE LAPSES COMMITTEE - PENALTY FEE-RS. 2500");
-		map.put("MLCPFR5000", "MALPRACTICE LAPSES COMMITTEE - PENALTY FEE-RS. 5000");
-		map.put("MLCPFR7500", "MALPRACTICE LAPSES COMMITTEE - PENALTY FEE-RS. 7500");
-		map.put("MLCPFR10000", "MALPRACTICE LAPSES COMMITTEE - PENALTY FEE-RS. 10000");
-		map.put("MLCPFR12500", "MALPRACTICE LAPSES COMMITTEE - PENALTY FEE-RS. 12500");
-		map.put("MLCPFR17500", "MALPRACTICE LAPSES COMMITTEE - PENALTY FEE-RS. 17500");
-		map.put("MLCPFR20000", "MALPRACTICE LAPSES COMMITTEE - PENALTY FEE-RS. 20000");
-		map.put("MLCPFR25000", "MALPRACTICE LAPSES COMMITTEE - PENALTY FEE-RS. 25000");
-		map.put("RTFS1", "RE-TOTALING FOR 1 SUBJECT");
-		map.put("RTFS2", "RE-TOTALING FOR 2 SUBJECT");
-		map.put("RTFS3", "RE-TOTALING FOR 3 SUBJECT");
-		map.put("RTFS4", "RE-TOTALING FOR 4 SUBJECT");
-		map.put("RTFS5", "RE-TOTALING FOR 5 SUBJECT");
-		map.put("PMCFFR5000", "PROFESSIONAL MISCONDUCT COMMITTEE FINE FEES-RS. 5000");
-		map.put("PMCFFR1000", "PROFESSIONAL MISCONDUCT COMMITTEE FINE FEES-RS. 10000");
-		map.put("PMCFFR20000", "PROFESSIONAL MISCONDUCT COMMITTEE FINE FEES-RS. 20000");
-		map.put("PMCFFR30000", "PROFESSIONAL MISCONDUCT COMMITTEE FINE FEES-RS. 30000");
-		map.put("PMCFFR50000", "PROFESSIONAL MISCONDUCT COMMITTEE FINE FEES-RS. 50000");
-		map.put("PMCFFR100000", "PROFESSIONAL MISCONDUCT COMMITTEE FINE FEES-RS. 100000");
-		/*Session session = factory.openSession();
-		Criteria criteria = session.createCriteria(ServiceTypeBean.class);
-		list = criteria.list();
-		Iterator<ServiceTypeBean> itr = list.iterator();
-		while (itr.hasNext()) {
-			ServiceTypeBean serviceTypeBean = (ServiceTypeBean) itr.next();
+		list = getServiceTypeDetailList();
+		Session session = factory.openSession();
+		if (list.isEmpty()) {
 
-			map.put(serviceTypeBean.getServiceKey(), serviceTypeBean.getServiceValue());
+			map.put("EC", "ELIGIBILITY CRITERIA");
+			map.put("ECR", "ELIGIBILITY CERTIFICATE RENEWAL");
+			map.put("CONAIE", "CHANGE OF NAME & INITIAL EXPANSION");
+			map.put("MC", "MIGRATION CERTIFICATE");
+			map.put("MT", "MIGRATION TRANSFER 1ST TO 2ND YEAR ");
+			map.put("NOCIT", "NOC(for Internship Transfer)");
+			map.put("CMCL", "CONSOLIDATED MARKS CARD (LAMINATED)");
+			map.put("NCIMC", "NAME CORRECTION IN MARKS CARD");
+			map.put("PDC", "PDC");
+			map.put("RC", "RANK CERTIFICATE");
+			map.put("MPC", "MEDAL/PRIZE CERTIFICATE");
+			map.put("MPCD", "MEDAL/PRIZE sCERTIFICATE-Duplicate");
+			map.put("DDC", "DUPLICATE DEGREE CERTIFICATE");
+			map.put("PGETS", "PGET Superspeciality");
+			map.put("PGETF", "PGET Fee");
+			map.put("PGETA", "PGET Application");
+			map.put("VO", "VERIFICATION ONLY");
+			map.put("VACOC", "VERIFICATION AND CERTIFICATION OF COPY'S");
+			map.put("CCOTSO", "CERTIFIED COPY OF THE SYLLABUS/ORINANCE");
+			map.put("DMC", "DUPLICATE MARKS CARD");
+			map.put("PPC", "PROVISIONAL PASS CERTIFICATE");
+			map.put("DC", "DEGREE CERTIFICATE");
+			map.put("CC", "CERTIFICATE COURSE");
+			map.put("NCIDC", "NAME CORRECTION IN DEGREE CERTIFICATE");
+			map.put("COTOTP", "CHANGE OF TITLE OF THESIS-PHD");
+			map.put("RRP", "RE-REGISTRATION-PH.D");
+			map.put("EOR", "EXTENSION OF REGISTRATION");
+			map.put("PE", "PRE EXAMINATION");
+			map.put("RF", "REGISTRATION FEE");
+			map.put("FSSAFPE", "FINAL SYNOPSIS SUBMISSION AND FINAL PH.D. EXAMINATION");
+			map.put("DF", "DISSERTATION FEE");
+			map.put("DFR", "DISSERTATION FEE-RESUBMISSION");
+			map.put("CVOTAOTDF", "CREDENTIAL VERIFICATION / OFFICIAL TRANSCRIPT/ ATTESTATION OF THE DOCUMENTS FEE");
+			map.put("MOIEC", "MEDIUM OF INSTRUCTION IN ENGLISH CERTIFICATION");
+			map.put("MLCPFR2500", "MALPRACTICE LAPSES COMMITTEE - PENALTY FEE-RS. 2500");
+			map.put("MLCPFR5000", "MALPRACTICE LAPSES COMMITTEE - PENALTY FEE-RS. 5000");
+			map.put("MLCPFR7500", "MALPRACTICE LAPSES COMMITTEE - PENALTY FEE-RS. 7500");
+			map.put("MLCPFR10000", "MALPRACTICE LAPSES COMMITTEE - PENALTY FEE-RS. 10000");
+			map.put("MLCPFR12500", "MALPRACTICE LAPSES COMMITTEE - PENALTY FEE-RS. 12500");
+			map.put("MLCPFR17500", "MALPRACTICE LAPSES COMMITTEE - PENALTY FEE-RS. 17500");
+			map.put("MLCPFR20000", "MALPRACTICE LAPSES COMMITTEE - PENALTY FEE-RS. 20000");
+			map.put("MLCPFR25000", "MALPRACTICE LAPSES COMMITTEE - PENALTY FEE-RS. 25000");
+			map.put("RTFS1", "RE-TOTALING FOR 1 SUBJECT");
+			map.put("RTFS2", "RE-TOTALING FOR 2 SUBJECT");
+			map.put("RTFS3", "RE-TOTALING FOR 3 SUBJECT");
+			map.put("RTFS4", "RE-TOTALING FOR 4 SUBJECT");
+			map.put("RTFS5", "RE-TOTALING FOR 5 SUBJECT");
+			map.put("PMCFFR5000", "PROFESSIONAL MISCONDUCT COMMITTEE FINE FEES-RS. 5000");
+			map.put("PMCFFR1000", "PROFESSIONAL MISCONDUCT COMMITTEE FINE FEES-RS. 10000");
+			map.put("PMCFFR20000", "PROFESSIONAL MISCONDUCT COMMITTEE FINE FEES-RS. 20000");
+			map.put("PMCFFR30000", "PROFESSIONAL MISCONDUCT COMMITTEE FINE FEES-RS. 30000");
+			map.put("PMCFFR50000", "PROFESSIONAL MISCONDUCT COMMITTEE FINE FEES-RS. 50000");
+			map.put("PMCFFR100000", "PROFESSIONAL MISCONDUCT COMMITTEE FINE FEES-RS. 100000");
 
-		}*/
+			ServiceTypeBean serviceTypeBean = new ServiceTypeBean();
 
-		/*
-		 * for (Map.Entry<String, String> entry : map.entrySet()) { Session
-		 * session = factory.openSession(); Transaction tx =
-		 * session.beginTransaction();
-		 * serviceTypeBean.setServiceKey(entry.getKey());
-		 * serviceTypeBean.setServiceValue(entry.getValue());
-		 * 
-		 * System.out.println("Key is ::" + entry.getKey() + "/" + "value is" +
-		 * entry.getValue()); session.save(serviceTypeBean); tx.commit();
-		 * 
-		 * }
-		 */
-		return map;
+			for (Map.Entry<String, String> entry : map.entrySet()) {
+
+				Transaction tx = session.beginTransaction();
+				serviceTypeBean.setServiceKey(entry.getKey());
+				serviceTypeBean.setServiceValue(entry.getValue());
+
+				System.out.println("Key is ::" + entry.getKey() + "/" + "value is" + entry.getValue());
+				session.save(serviceTypeBean);
+				tx.commit();
+
+			}
+			return map;
+
+		} else {
+			Criteria criteria = session.createCriteria(ServiceTypeBean.class);
+			list = criteria.list();
+			Iterator<ServiceTypeBean> itr = list.iterator();
+			while (itr.hasNext()) {
+				ServiceTypeBean serviceTypeBean = (ServiceTypeBean) itr.next();
+
+				map.put(serviceTypeBean.getServiceKey(), serviceTypeBean.getServiceValue());
+
+			}
+			return map;
+
+		}
+
+	}
+
+	// get List Of faculty if exists
+	private List<FacultyBean> getFacultyDetailList() {
+
+		Session session = factory.openSession();
+		try {
+			Criteria criteria = session.createCriteria(FacultyBean.class);
+			List<FacultyBean> list = criteria.list();
+			return list;
+		} finally {
+			session.close();
+			// TODO: handle exception
+		}
 
 	}
 
@@ -141,72 +180,155 @@ public class ApplicantFeeCollectionDAO {
 
 		Map<String, String> map = new HashMap<String, String>();
 		List<FacultyBean> list = new ArrayList<FacultyBean>();
-		map.put("MED", "MEDICAL");
-		map.put("DENT", "DENTAL");
-		map.put("NursPHYSIOPHARM", "NURSING,PHYSIOTHERAPY,PHARMACY");
-		map.put("AYAVRDHOMEOUNANYOGA", "AYUSH,AYURVEDA,HOMEOPATHY,UNANI,YOGA");
-		map.put("PMEDIAOTHERS", "PARAMEDICAL AND Others");
+		Session session = factory.openSession();
+		if (list.isEmpty()) {
 
-		/*Session session = factory.openSession();
-		Criteria criteria = session.createCriteria(FacultyBean.class);
-		list = criteria.list();
-		Iterator<FacultyBean> itr = list.iterator();
-		while (itr.hasNext()) {
-			FacultyBean serviceTypeBean = (FacultyBean) itr.next();
+			map.put("MED", "MEDICAL");
+			map.put("DENT", "DENTAL");
+			map.put("NursPHYSIOPHARM", "NURSING,PHYSIOTHERAPY,PHARMACY");
+			map.put("AYAVRDHOMEOUNANYOGA", "AYUSH,AYURVEDA,HOMEOPATHY,UNANI,YOGA");
+			map.put("PMEDIAOTHERS", "PARAMEDICAL AND Others");
 
-			map.put(serviceTypeBean.getFacultyKey(), serviceTypeBean.getFacultyValue());
+			FacultyBean facultyBean = new FacultyBean();
 
-		}*/
-		return map;
+			for (Map.Entry<String, String> entry : map.entrySet()) {
+
+				Transaction tx = session.beginTransaction();
+				facultyBean.setFacultyKey(entry.getKey());
+				facultyBean.setFacultyValue(entry.getValue());
+
+				System.out.println("Key is ::" + entry.getKey() + "/" + "value is" + entry.getValue());
+				session.save(facultyBean);
+				tx.commit();
+
+			}
+			return map;
+
+		} else {
+
+			Criteria criteria = session.createCriteria(FacultyBean.class);
+			list = criteria.list();
+			Iterator<FacultyBean> itr = list.iterator();
+			while (itr.hasNext()) {
+				FacultyBean serviceTypeBean = (FacultyBean) itr.next();
+
+				map.put(serviceTypeBean.getFacultyKey(), serviceTypeBean.getFacultyValue());
+
+			}
+			return map;
+		}
+	}
+
+	// get List Of faculty if exists
+	private List<NationalityBean> getNationalityDetailList() {
+
+		Session session = factory.openSession();
+		try {
+			Criteria criteria = session.createCriteria(NationalityBean.class);
+			List<NationalityBean> list = criteria.list();
+			return list;
+		} finally {
+			session.close();
+			// TODO: handle exception
+		}
 
 	}
 
 	public Map<String, String> nationalityList() {
 
 		Map<String, String> map = new HashMap<String, String>();
-
-		map.put("INDIAN", "INDIAN");
-		map.put("NRISAARC", "NRI/SAARC");
-		map.put("FOREIGN", "FOREIGN");
-
-		/*Session session = factory.openSession();
+		Session session = factory.openSession();
 		List<NationalityBean> list = new ArrayList<NationalityBean>();
-		Criteria criteria = session.createCriteria(NationalityBean.class);
-		list = criteria.list();
-		Iterator<NationalityBean> itr = list.iterator();
-		while (itr.hasNext()) {
-			NationalityBean serviceTypeBean = (NationalityBean) itr.next();
+		if (list.isEmpty()) {
+			map.put("INDIAN", "INDIAN");
+			map.put("NRISAARC", "NRI/SAARC");
+			map.put("FOREIGN", "FOREIGN");
 
-			map.put(serviceTypeBean.getNationalityKey(), serviceTypeBean.getNationalityValue());
+			NationalityBean nationalityBean = new NationalityBean();
+			for (Map.Entry<String, String> entry : map.entrySet()) {
 
-		}*/
-		return map;
+				Transaction tx = session.beginTransaction();
+				nationalityBean.setNationalityKey(entry.getKey());
+				nationalityBean.setNationalityValue(entry.getValue());
+
+				System.out.println("Key is ::" + entry.getKey() + "/" + "value is" + entry.getValue());
+				session.save(nationalityBean);
+				tx.commit();
+
+			}
+			return map;
+		}
+
+		else {
+
+			Criteria criteria = session.createCriteria(NationalityBean.class);
+			list = criteria.list();
+			Iterator<NationalityBean> itr = list.iterator();
+			while (itr.hasNext()) {
+				NationalityBean serviceTypeBean = (NationalityBean) itr.next();
+
+				map.put(serviceTypeBean.getNationalityKey(), serviceTypeBean.getNationalityValue());
+
+			}
+
+			return map;
+		}
+	}
+
+	// get List Of faculty if exists
+	private List<CourseBean> getCourseDetailList() {
+
+		Session session = factory.openSession();
+		try {
+			Criteria criteria = session.createCriteria(CourseBean.class);
+			List<CourseBean> list = criteria.list();
+			return list;
+		} finally {
+			session.close();
+			// TODO: handle exception
+		}
 
 	}
 
 	public Map<String, String> courseList() {
 
 		Session session = factory.openSession();
-		CourseBean facultyBean = new CourseBean();
+		CourseBean courseBean = new CourseBean();
 		Map<String, String> map = new HashMap<String, String>();
 
-		map.put("PG", "PG");
-		map.put("UG", "UG");
-		map.put("PGDIPLOMA", "PG DIPLOMA");
-		map.put("PHD", "PHD");
+		List<CourseBean> list = new ArrayList<CourseBean>();
+		if (list.isEmpty()) {
+			map.put("PG", "PG");
+			map.put("UG", "UG");
+			map.put("PGDIPLOMA", "PG DIPLOMA");
+			map.put("PHD", "PHD");
 
-		/*List<CourseBean> list = new ArrayList<CourseBean>();
-		Criteria criteria = session.createCriteria(CourseBean.class);
-		list = criteria.list();
-		Iterator<CourseBean> itr = list.iterator();
-		while (itr.hasNext()) {
-			CourseBean serviceTypeBean = (CourseBean) itr.next();
+			for (Map.Entry<String, String> entry : map.entrySet()) {
 
-			map.put(serviceTypeBean.getCourseKey(), serviceTypeBean.getCourseValue());
+				Transaction tx = session.beginTransaction();
+				courseBean.setCourseKey(entry.getKey());
+				courseBean.setCourseValue(entry.getValue());
 
-		}*/
+				System.out.println("courseBean" + entry.getKey() + "/" + "value is" + entry.getValue());
+				session.save(courseBean);
+				tx.commit();
 
-		return map;
+			}
+			return map;
+
+		} else {
+			Criteria criteria = session.createCriteria(CourseBean.class);
+			list = criteria.list();
+			Iterator<CourseBean> itr = list.iterator();
+			while (itr.hasNext()) {
+				CourseBean serviceTypeBean = (CourseBean) itr.next();
+
+				map.put(serviceTypeBean.getCourseKey(), serviceTypeBean.getCourseValue());
+
+			}
+
+			return map;
+		}
 
 	}
 
